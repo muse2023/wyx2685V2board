cbc2ca07d41769059ac8ab33f444d9938ef0b1c5
muse2023 <pandasuperfans@gmail.com>
2025-04-18 05:22:32 +0800

    修改路由2

--- routes/web.php
+++ routes/web.php
@@ -283,7 +283,7 @@ Route::get('/user', function (Request $request) {
     }
     $renderParams = [
         'title' => config('v2board.app_name', 'V2Board'),
-        'theme' => config('v2board.frontend_theme', 'default'),、
+        'theme' => config('v2board.frontend_theme', 'default'),
         'theme_path' => '/theme/' . config('v2board.frontend_theme', 'v2board') . '/assets/',
         'version' => config('app.version'),
         'description' => config('v2board.app_description', 'V2Board is best'),
